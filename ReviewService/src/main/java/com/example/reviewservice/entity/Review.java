e008a8c0aa5aa53d995f3941582e02e1931f3e7e
i-anandsingh <singh.anand974@gmail.com>
2024-06-18 00:09:14 +0530

    Review Entity created

---
+++ ReviewService/src/main/java/com/example/reviewservice/entity/Review.java
@@ -0,0 +1,33 @@
+package com.example.reviewservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "review_table")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    private Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)   // annotation tells spring about the format of the Date object to be stored i.e. Date/ Time/ Timestamp
+    @CreatedDate    // annotation tells spring only handle at object creation
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate   // annotation tells spring only handle at object updation
+    private Date updatedAt;
+}
