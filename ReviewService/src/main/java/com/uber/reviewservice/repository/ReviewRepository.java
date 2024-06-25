63ad50e1588a57302d74eca9cbc6e708453d2ef5
i-anandsingh <singh.anand974@gmail.com>
2024-06-25 22:46:11 +0530

    flyway init setup

--- ReviewService/src/main/java/com/uber/reviewservice/repository/ReviewRepository.java
+++ ReviewService/src/main/java/com/uber/reviewservice/repository/ReviewRepository.java
@@ -1,9 +1,9 @@
 package com.uber.reviewservice.repository;
 
-import com.uber.reviewservice.entity.ReviewEntity;
+import com.uber.reviewservice.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<ReviewEntity, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }
