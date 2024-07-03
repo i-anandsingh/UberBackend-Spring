fd0f4b053323e01c7025ca5d951d7cd10f4ef1dc
i-anandsingh <singh.anand974@gmail.com>
2024-07-03 20:36:41 +0530

    added enableWebSecurity annotation in springsecurity

--- AuthService/src/main/java/com/uber/service/auth/configs/Security.java
+++ AuthService/src/main/java/com/uber/service/auth/configs/Security.java
@@ -3,10 +3,12 @@ package com.uber.service.auth.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class Security{
 
     //csrf : cross site request forgery
