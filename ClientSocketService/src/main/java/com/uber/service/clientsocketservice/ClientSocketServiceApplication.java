74720e57205a08dd15f825fa2c61f8a8a2ba7009
i-anandsingh <singh.anand974@gmail.com>
2024-07-18 01:04:23 +0530

    Initialized ClientSocketService

---
+++ ClientSocketService/src/main/java/com/uber/service/clientsocketservice/ClientSocketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uber.service.clientsocketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSocketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientSocketServiceApplication.class, args);
+    }
+
+}
---
+++ ClientSocketService/src/test/java/com/uber/service/clientsocketservice/ClientSocketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.service.clientsocketservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSocketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
