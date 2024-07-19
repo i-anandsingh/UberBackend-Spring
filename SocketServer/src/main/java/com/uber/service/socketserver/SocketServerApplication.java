8f22426aaa093fcafe8fc605cb3ab405b496c12d
i-anandsingh <singh.anand974@gmail.com>
2024-07-19 23:12:29 +0530

    Socket Server initialized

---
+++ SocketServer/src/main/java/com/uber/service/socketserver/SocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.uber.service.socketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketServerApplication.class, args);
+    }
+
+}
